b249175f5e6a28fbb7f2a64dedb5e93f9bc22a9c
Mariam Khurshudyan <mariamarm98@gmail.com>
2021-07-15 04:19:04 +0400

    Adding new to-do-card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { TodoCardComponent } from './todo-card/todo-card.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
+
+
 export class AppComponent {
   title = 'epam-course-angular-application';
 }
